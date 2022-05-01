ee2fa7689f7013b75af78251b24a6544f0da3e58
icarus8050 <icarus8050@naver.com>
2022-05-01 20:28:53 +0900

    경계 테스트하기

--- chapter_04/ProvinceTest.js
+++ chapter_04/ProvinceTest.js
@@ -20,5 +20,50 @@ describe('province', function () {
 
     it('profit', function() {
         expect(asia.profit).equal(230);
-    })
+    });
+
+    it('change production', function() {
+        asia.producers[0].production = 20;
+        expect(asia.shortfall).equal(-6);
+        expect(asia.profit).equal(292);
+    });
+
+    it('zero demand', function () { // 수요가 없다.
+        asia.demand = 0;
+        expect(asia.shortfall).equal(-25);
+        expect(asia.profit).equal(0);
+    });
+
+    it('negative demand', function () { // 수요가 마이너스다.
+        asia.demand = -1;
+        expect(asia.shortfall).equal(-26);
+        expect(asia.profit).equal(-10);
+    });
+
+    it('empty string demand', function () { // 수요 입력란이 비어있다.
+        asia.demand = "";
+        expect(asia.shortfall).NaN;
+        expect(asia.profit).NaN;
+    });
+});
+
+describe('no producers', function() {   // 생산자가 없는 경우
+    let noProducers;
+    beforeEach(function () {
+        const data = {
+            name: "No producers",
+            producers: [],
+            demand: 30,
+            price: 20,
+        };
+        noProducers = new Province(data);
+    });
+
+    it('shortfall', function() {
+        expect(noProducers.shortfall).equal(30);
+    });
+
+    it('profit', function () {
+        expect(noProducers.profit).equal(0);
+    });
 });
